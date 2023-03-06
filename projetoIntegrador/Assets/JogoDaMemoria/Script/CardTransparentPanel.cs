01d950f85d77f26179ef5217adb62344d3cfa673
AnaGraziela <graziela27112005@gmail.com>
2023-03-06 15:44:28 -0400

    Commit11

--- projetoIntegrador/Assets/JogoDaMemoria/Script/CardTransparentPanel.cs
+++ projetoIntegrador/Assets/JogoDaMemoria/Script/CardTransparentPanel.cs
@@ -9,23 +9,40 @@ using UnityEngine.UI;
 
 public class CardTransparentPanel : MonoBehaviour
 {
+    public Button button;
     public GameObject a;
     public Image transparentImage;
     public Image image;
-    public float duration = 2f;
+    public float duration;
 
+    private void Start()
+    {
+        button.enabled = false;
+        GameController gameController = a.GetComponent<GameController>();
+        gameController.listImageSelected.Clear();
+        gameController.listTransparentPanelSelected.Clear();
+        gameController.listButton.Clear();
+        transparentImage.color = new Color(transparentImage.color.r, transparentImage.color.g, transparentImage.color.b, 0);
+        Invoke("Cards", duration);
+    }
+    public void Cards()
+    {
+        transparentImage.color = new Color(transparentImage.color.r, transparentImage.color.g, transparentImage.color.b, 1);
+        button.enabled = true;
+    }
     public void OnClick()
     {
+
         GameController gameController = a.GetComponent<GameController>();
 
         transparentImage.color = new Color(transparentImage.color.r, transparentImage.color.g, transparentImage.color.b, 0);
         gameController.listImageSelected.Add(image);
         gameController.listTransparentPanelSelected.Add(transparentImage);
+        gameController.listButton.Add(button);
 
         Debug.Log("Lista: " + gameController.listImageSelected.Count);
 
-
-        if (gameController.listImageSelected.Count >= 2)
+        if (gameController.listImageSelected.Count == 2)
         {
             Image im1 = gameController.listImageSelected[0];
             Image im2 = gameController.listImageSelected[1];
@@ -34,18 +51,24 @@ public class CardTransparentPanel : MonoBehaviour
 
             if (sprite1.name == sprite2.name)
             {
+                Button b1 = gameController.listButton[0];
+                Button b2 = gameController.listButton[1];
+                b1.enabled = false;
+                b2.enabled = false;
                 Debug.Log("Acertou!");
+                im1.color = new Color(im1.color.r, im1.color.g, im1.color.b, 0.5f);
+                im2.color = new Color(im2.color.r, im2.color.g, im2.color.b, 0.5f);
                 gameController.listImageSelected.Clear();
                 gameController.listTransparentPanelSelected.Clear();
+                gameController.listButton.Clear();
             }
             else
             {
                 Debug.Log("Errou!");
                 gameController.listImageSelected.Clear();
+                gameController.listButton.Clear();
                 Invoke("ResetAlpha", duration);
             }
-            Invoke("ResetAlpha", duration);
-
         }
     }
     public void ResetAlpha()
@@ -53,9 +76,8 @@ public class CardTransparentPanel : MonoBehaviour
         GameController gameController = a.GetComponent<GameController>();
 
         Image transparent1 = gameController.listTransparentPanelSelected[0];
-        transparent1.color = new Color(transparentImage.color.r, transparentImage.color.g, transparentImage.color.b, 1);
-        gameController.listTransparentPanelSelected.Clear();
-
+        transparent1.color = new Color(transparent1.color.r, transparent1.color.g, transparent1.color.b, 1);
         transparentImage.color = new Color(transparentImage.color.r, transparentImage.color.g, transparentImage.color.b, 1);
+        gameController.listTransparentPanelSelected.Clear();
     }
 }
--- projetoIntegrador/Assets/JogoDaMemoria/Script/GameController.cs
+++ projetoIntegrador/Assets/JogoDaMemoria/Script/GameController.cs
@@ -58,7 +58,9 @@ public class GameController : MonoBehaviour
     public List<Image> cardList;
     public List<Image> listImageSelected = null;
     public List<Image> listTransparentPanelSelected = null;
+    public List<Button> listButton = null;
 
+    public float space;
     public int numRows = 0;
     public int numCols = 0;
     public int[] imageCount;
@@ -70,7 +72,7 @@ public class GameController : MonoBehaviour
     CardTransparentPanel cardTransparentPanel;
 
     Dictionary<Sprite, int> spritesAddList = new Dictionary<Sprite, int>();
-    
+
     private void Start()
     {
         imageCount = new int[cardImages.Length];
@@ -83,25 +85,65 @@ public class GameController : MonoBehaviour
         {
             for (int col = 0; col < numCols; col++)
             {
-                Vector3 pos = new Vector3(col - numCols / 2f + 0.5f, row - numRows / 2f + 0.5f, 0) * 65f;
+                Vector3 pos = new Vector3(col - numCols / 2f + 0.5f, row - numRows / 2f + 0.5f, 0) * space;
                 Image newCard = Instantiate(card, cards);
                 newCard.transform.localPosition = pos;
                 cardList.Add(newCard);
             }
         }
-        int im = cardImages.Length;
-        int ca = cardList.Count;
-        if (im > ca)
+         int im = cardImages.Length;
+         int ca = cardList.Count;
+         if (im > ca / 2)
+         {
+             Debug.Log("menor i: " + im + "c" + ca);
+             int a = ca / 2;
+             a = im - a;
+             for (int j = 0; j < a; j++)
+             {
+                 cardImages[j] = null;
+             }
+         }
+        // Criar uma lista para armazenar os índices das imagens disponíveis
+        List<int> availableImageIndices = new List<int>();
+        for (int i = 0; i < cardImages.Length; i++)
         {
-            Debug.Log("menor i: " + im + "c" + ca);
-            int a = im - ca;
-            for (int j = 0; j < a; j++)
+            if (cardImages[i] != null)
             {
-                cardImages[j] = null;
+                availableImageIndices.Add(i);
             }
         }
 
-        for (int i = 0; i < cardList.Count; i++)
+        // Percorrer as cartas e atribuir imagens aleatoriamente
+        foreach (var card in cardList)
+        {
+            // Selecionar aleatoriamente um índice da lista de imagens disponíveis
+            int randomIndex = UnityEngine.Random.Range(0, availableImageIndices.Count);
+            int imageIndex = availableImageIndices[randomIndex];
+
+            // Atribuir a imagem correspondente à carta atual
+            card.sprite = cardImages[imageIndex];
+
+            // Verificar se a imagem selecionada já foi usada várias vezes
+            if (spritesAddList.ContainsKey(card.sprite) && spritesAddList[card.sprite] >= 1)
+            {
+                // Remover o índice da lista de imagens disponíveis
+                availableImageIndices.RemoveAt(randomIndex);
+            }
+            else
+            {
+                // Adicionar a imagem ao dicionário de contagem de ocorrências
+                if (spritesAddList.ContainsKey(card.sprite))
+                {
+                    spritesAddList[card.sprite]++;
+                }
+                else
+                {
+                    spritesAddList.Add(card.sprite, 1);
+                }
+            }
+        }
+
+        /*for (int i = 0; i < cardList.Count; i++)
         {
             Sprite randomCardImage;
             int cardImageIndex;
@@ -134,6 +176,6 @@ public class GameController : MonoBehaviour
                 }
             }
             cardList[i].sprite = randomCardImage;
-        }
+        }*/
     }
 }
