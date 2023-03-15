dbc68a8371211e4b3753a79c8821a38f8bffce12
Augusto Anguita <wafosso2@gmail.com>
2023-03-15 11:12:20 -0400

    jogo da coleta
    implementações (ainda nao resolvi o score)

--- projetoIntegrador/Assets/Jogos/JogoDaColeta/Scripts/ObjectGenerator.cs
+++ projetoIntegrador/Assets/Jogos/JogoDaColeta/Scripts/ObjectGenerator.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class ObjectGenerator : MonoBehaviour
 {
     public GameObject Objects;
-    public float fallingInterval = 5f;
 
     void Start()
     {
-        InvokeRepeating("GenerateObject", 0f, fallingInterval);
+        StartCoroutine(GenerateObject());
     }
 
-    void GenerateObject()
+    IEnumerator GenerateObject()
     {
-        float x = Random.Range(-8f, 8f);
-        float y = 7f;
-        Vector2 initialPosition = new Vector2(x, y);
-        Instantiate(Objects, initialPosition, Quaternion.identity);
+        while (true)
+        {
+            float x = Random.Range(-8f, 8f);
+            float y = 7f;
+            Vector2 initialPosition = new Vector2(x, y);
+            Instantiate(Objects, initialPosition, Quaternion.identity);
+            yield return new WaitForSeconds(2.5f);
+        }
     }
  }
