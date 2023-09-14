e5811ba7a338b6b7e96079921a9d2d71c70bc13e
MellySabrina <graziela27112005@gmail.com>
2023-09-14 19:57:35 -0400

    Continuacao dos creditos

---
+++ projetoIntegrador/Assets/Scripts/CreditsScripts/ArrivedCharacter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrivedCharacter : MonoBehaviour
+{
+    public Canvas popUpInformations;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Debug.Log("Apertou enter");//TODO
+                popUpInformations.gameObject.SetActive(true);
+
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Saiu"); //TODO
+            popUpInformations.gameObject.SetActive(false);
+
+
+        }
+    }
+}
