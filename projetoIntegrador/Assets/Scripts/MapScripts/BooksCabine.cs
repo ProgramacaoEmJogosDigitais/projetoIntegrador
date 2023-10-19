68d185368304b324d1846432f683c0f9b293c94b
Kanhamolax <114705551+Kanhamolax@users.noreply.github.com>
2023-10-18 21:26:05 -0400

    pontos de livro

--- projetoIntegrador/Assets/Scripts/MapScripts/BooksCabine.cs
+++ projetoIntegrador/Assets/Scripts/MapScripts/BooksCabine.cs
@@ -4,80 +4,61 @@ using UnityEngine;
 
 public class BooksCabine : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> books;
+    [SerializeField] private List<GameObject> books,booksJump;
+    private int whater, quiz, run, headD;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BooksPonts.pJigsaw = 4;
+
+        if (BooksPonts.pJigsaw==4)
+        {
+           
+                books[0].SetActive(true);
+                StartCoroutine(Wait(booksJump[0]));
+                
+           
+        }
+
+        if(BooksPonts.pQuiz==4)
+        {
+           
+                books[1].SetActive(true);
+                StartCoroutine(Wait(booksJump[1]));
+               
+            
+        }
+
+        if (BooksPonts.pParkRun==4)
+        {
+           
+                books[2].SetActive(true);
+                StartCoroutine(Wait(booksJump[2]));
+              
+            
+        }
+
+      if(BooksPonts.pWaterfish==4)
+        {
+           
+                books[3].SetActive(true);
+                StartCoroutine(Wait(booksJump[3]));
+               
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (BooksPonts.pJigsaw)
-        {
-            case 1:
-                books[0].SetActive(true);
-                break;
-            case 2:
-                books[1].SetActive(true);
+        
 
-                break;
-            case 3:
-                books[2].SetActive(true);
-                break;
-            case 4:
-                books[3].SetActive(true);
-                break;
-        }
-
-        switch (BooksPonts.pQuiz)
-        {
-            case 1:
-                books[4].SetActive(true);
-                break;
-            case 2:
-                books[5].SetActive(true);
-                break;
-            case 3:
-                books[6].SetActive(true);
-                break;
-            case 4:
-                books[7].SetActive(true);
-                break;
-        }
-
-        switch (BooksPonts.pParkRun)
-        {
-            case 1:
-                books[8].SetActive(true);
-                break;
-            case 2:
-                books[9].SetActive(true);
-                break;
-            case 3:
-                books[10].SetActive(true);
-                break;
-            case 4:
-                books[11].SetActive(true);
-                break;
-        }
-
-        switch (BooksPonts.pWaterfish)
-        {
-            case 1:
-                books[12].SetActive(true);
-                break;
-            case 2:
-                books[13].SetActive(true);
-                break;
-            case 3:
-                books[14].SetActive(true);
-                break;
-            case 4:
-                books[15].SetActive(true);
-                break;
-        }
-
+    }
+    private IEnumerator Wait(GameObject  obj)
+    {
+        yield return new WaitForSeconds(3);
+        obj.SetActive(true);
+            yield return new WaitForSeconds(1.2f);
+            obj.SetActive(false);
+        
     }
 }
