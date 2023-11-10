b8fac5ce2bb1aa731937f3191a08413d5ef392b7
filipin04 <filipelima20062701@gmail.com>
2023-11-09 21:40:53 -0400

    começando a implementação do Quiz
    comecei a fazer a logica.

---
+++ projetoIntegrador/Assets/Scripts/QuizScripts/GameController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject cube1;
+    public GameObject cube2;
+    public GameObject cube3;
+    public GameObject cube4;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
