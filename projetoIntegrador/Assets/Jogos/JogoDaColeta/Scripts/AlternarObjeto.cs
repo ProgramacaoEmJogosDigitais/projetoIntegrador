2c10aceca99a10912198bde6e4f124f4cdb72454
Mauriiicio <mauricio1desouza@gmail.com>
2023-11-09 23:49:43 -0400

    Implementacao jogo da coleta

---
+++ projetoIntegrador/Assets/Jogos/JogoDaColeta/Scripts/AlternarObjeto.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlternarObjeto : MonoBehaviour
+{
+    public GameObject objeto1;
+    public GameObject objeto2;
+    private bool ativarObjeto1 = true;
+
+    void Start()
+    {
+        AtivarObjetos();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ativarObjeto1 = !ativarObjeto1;
+            AtivarObjetos();
+        }
+    }
+
+    void AtivarObjetos()
+    {
+        if (ativarObjeto1)
+        {
+            objeto1.GetComponent<SpriteRenderer>().enabled = true;
+            objeto1.GetComponent<Collider2D>().enabled = true;
+            objeto2.GetComponent<SpriteRenderer>().enabled = false;
+            objeto2.GetComponent<Collider2D>().enabled = false;
+        }
+        else
+        {
+            objeto1.GetComponent<SpriteRenderer>().enabled = false;
+            objeto1.GetComponent<Collider2D>().enabled = false;
+            objeto2.GetComponent<SpriteRenderer>().enabled = true;
+            objeto2.GetComponent<Collider2D>().enabled = true;
+        }
+    }
+}
