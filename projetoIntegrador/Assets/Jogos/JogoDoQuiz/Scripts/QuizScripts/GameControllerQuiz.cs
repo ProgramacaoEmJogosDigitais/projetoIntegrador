e25b6cf22096f115d977d14cfad2c2a60f057656
filipin04 <filipelima20062701@gmail.com>
2023-12-05 21:08:33 -0400

    bugs game over
    não deixando selecionar as alternativas enquanto estiver no game over.

--- projetoIntegrador/Assets/Jogos/JogoDoQuiz/Scripts/QuizScripts/GameControllerQuiz.cs
+++ projetoIntegrador/Assets/Jogos/JogoDoQuiz/Scripts/QuizScripts/GameControllerQuiz.cs
@@ -17,7 +17,10 @@ public class GameControllerQuiz : MonoBehaviour
     public Sprite vidaUsada;
     public Sprite[] personagens;
     public Image PontoP;
-  
+    public GameObject painelGameOver;
+    public GameObject painelBackground;
+
+
     public TMP_Text pergunta;
     public TMP_Text respostaA;
     public TMP_Text respostaB;
@@ -47,6 +50,7 @@ public class GameControllerQuiz : MonoBehaviour
     private float erros;
     private bool verificacao_acerto;
     private bool verificacao_pergunta = true;
+    
 
     void Start()
     {
@@ -99,8 +103,9 @@ public class GameControllerQuiz : MonoBehaviour
         if (indicesUtilizados.Count == perguntas.Length)
         {
             //TODO Talvez reiniciar o jogo, aqui ja foram todas as perguntas.
-            GameOver();
-            StartCoroutine(Espera2());
+           
+            pontuacao.text = "Perguntas Corretas : " + acertos.ToString() + "/" + questoes.ToString();
+            gameOverQuiz.SetActive(true);
             indicesUtilizados.Clear();
             //ShuffleQuestions();
         }
@@ -226,6 +231,9 @@ public class GameControllerQuiz : MonoBehaviour
         if (erros == 3)
         {
             over = true;
+            
+            painelGameOver.SetActive(true);
+            
             StartCoroutine(Espera2());
             pontuacao.text = "Perguntas Corretas : " + acertos.ToString() + "/" + questoes.ToString();
         }
@@ -266,7 +274,9 @@ public class GameControllerQuiz : MonoBehaviour
             }
             */
 
-            ShowQuestion(); 
+            ShowQuestion();
+         
+           
         }
     }
 
@@ -285,8 +295,8 @@ public class GameControllerQuiz : MonoBehaviour
 
     private IEnumerator Espera2()
     {
-        yield return new WaitForSeconds(1);
-        gameOverQuiz.SetActive(true);
+        yield return new WaitForSeconds(0); ;
+        
     }
 
     public int AchaCerta()
