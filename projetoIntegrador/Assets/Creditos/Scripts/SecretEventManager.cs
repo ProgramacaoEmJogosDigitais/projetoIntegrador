6a76b3ac172ff67ffcc38e7e89fc8519d1db04ea
Mauriiicio <mauricio1desouza@gmail.com>
2024-03-08 01:15:33 -0400

    Adicionado fotos e info
    Adicionado foto e nome nos creditos.

--- projetoIntegrador/Assets/Creditos/Scripts/SecretEventManager.cs
+++ projetoIntegrador/Assets/Creditos/Scripts/SecretEventManager.cs
@@ -4,57 +4,78 @@ public class SecretEventManager : MonoBehaviour
 {
     private int clickCountObject1 = 0;
     private int clickCountObject2 = 0;
-    public bool fight = false;
-    public bool fight2 = false;
+    private bool fight = false;
+    private bool fight2 = false;
+    public GameObject juliano;
+    public GameObject ana;
+    public Sprite julianoAngry;
+    public Sprite anaAngry;
+    public Sprite julianoPosFight;
+    public Sprite anaPosFight;
+    private float animationDuration = 8f;
+    private float timer = 0f;
+
     void Update()
     {
-       
         if (Input.GetMouseButtonDown(0))
         {
-            
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
 
-         
             if (hit.collider != null)
             {
-             
                 if (hit.collider.gameObject.CompareTag("Ana"))
                 {
-                   
                     clickCountObject1++;
-
-                   
-                    if (clickCountObject1 == 3)
+                    if (clickCountObject1 == 10)
                     {
                         fight = true;
+                        clickCountObject1 = 0;
                     }
                 }
                 else if (hit.collider.gameObject.CompareTag("Juliano"))
                 {
-                   
                     clickCountObject2++;
-
-               
-                    if (clickCountObject2 == 4)
+                    if (clickCountObject2 == 10)
                     {
                         fight2 = true;
+                        clickCountObject2 = 0;
                     }
                 }
             }
         }
-        if(fight && fight2)
+
+        if (fight && fight2)
         {
             CallAnimation();
             fight = false;
             fight2 = false;
+            timer = 0f;
+        }
+
+        if (timer >= animationDuration)
+        {
+            DisableAnimations();
+        }
+        else
+        {
+            timer += Time.deltaTime;
         }
     }
 
     void CallAnimation()
     {
-        Debug.Log("Chamou o evento secreto para o objeto 1");
+        juliano.GetComponent<Animator>().SetBool("FightAna", true);
+        juliano.GetComponent<SpriteRenderer>().sprite = julianoAngry;
+        ana.GetComponent<SpriteRenderer>().sprite = anaAngry;
+        ana.GetComponent<Animator>().SetBool("FightJuliano", true);
     }
 
-    
+    void DisableAnimations()
+    {
+        juliano.GetComponent<Animator>().SetBool("FightAna", false);
+        ana.GetComponent<Animator>().SetBool("FightJuliano", false);
+        juliano.GetComponent<SpriteRenderer>().sprite = julianoPosFight;
+        ana.GetComponent<SpriteRenderer>().sprite = anaPosFight;
+    }
 }
