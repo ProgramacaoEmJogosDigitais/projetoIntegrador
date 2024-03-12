1d50ef458485b2393dc809ffe6c8722f4017342c
Mauriiicio <mellysabrinaa01@gmail.com>
2024-03-11 21:42:56 -0400

    CONTAGEM REGRESSIVA
    estou mudando os sitema de contagem regressiva (nao completo)

--- projetoIntegrador/Assets/Jogos/JogoDaCorrida/Scripts/PauseJCorrida.cs
+++ projetoIntegrador/Assets/Jogos/JogoDaCorrida/Scripts/PauseJCorrida.cs
@@ -11,7 +11,7 @@ public class PauseJCorrida : MonoBehaviour
     public bool gamePaused;
     public bool pressButtonNoPause; //caso obotao de despausar seja apertado, a contagem regressiva é ativada
     public Button bt_pause;
-    public float time;
+    public int time;
     public TMP_Text txt_Countdown;
    // public bool pauseWithP;
 
@@ -23,31 +23,31 @@ public class PauseJCorrida : MonoBehaviour
     private void Start()
     {
         panelPause.SetActive(false);
-        time = 3.0f;
+        time = 3;
 
     }
 
     private void Update()
     {
-        if (pressButtonNoPause)
-        {
-            time -= 0.003f;
-            txt_Countdown.text = time.ToString("F0");
-            if (time <= 0.0f)
-            {
-                pressButtonNoPause = false;
-                txt_Countdown.gameObject.SetActive(false);
-                gamePaused = false;
-                //Time.timeScale = 1f;
-                time = 3.0f;
-                bt_pause.gameObject.SetActive(true);
+        //if (pressButtonNoPause)
+        //{
+        //    time -= 0.003f;
+        //    txt_Countdown.text = time.ToString("F0");
+        //    if (time <= 0.0f)
+        //    {
+        //        pressButtonNoPause = false;
+        //        txt_Countdown.gameObject.SetActive(false);
+        //        gamePaused = false;
+        //        //Time.timeScale = 1f;
+        //        time = 3.0f;
+        //        bt_pause.gameObject.SetActive(true);
 
-            }
-            else
-            {
-                pressButtonNoPause = true;
-            }
-        }
+        //    }
+        //    else
+        //    {
+        //        pressButtonNoPause = true;
+        //    }
+        //}
 
         //if (Input.GetKeyDown(KeyCode.P) && pauseWithP && !pressButtonNoPause) //se o jogo nao tiver pausado, ele pode apertar o pause
         //{
@@ -79,6 +79,7 @@ public class PauseJCorrida : MonoBehaviour
         panelPause.gameObject.SetActive(false);
         txt_Countdown.gameObject.SetActive(true);
         pressButtonNoPause = true;
+        StartCoroutine(Countdown());
 
     }
     public void GoMap(string sceneName)
@@ -90,6 +91,29 @@ public class PauseJCorrida : MonoBehaviour
     {
         pressButtonNoPause = true;
         gamePaused = true;
+    }
+
+    private IEnumerator Countdown()//contador regressivo
+
+    {
+        for (int i = time; i >= 0; i--)
+        {
+            Debug.Log("ENTROUUU, valor do time: " + time);
+            txt_Countdown.text = time.ToString("F0");
+            Debug.Log(txt_Countdown.text = time.ToString("F0"));
+            yield return new WaitForSeconds(1);
+
+            if (i <= 0)
+            {
+                pressButtonNoPause = false;
+                txt_Countdown.gameObject.SetActive(false);
+                gamePaused = false;
+                time = 3;
+                bt_pause.gameObject.SetActive(true);
+
+            }
+        }
+       
     }
 
 }
